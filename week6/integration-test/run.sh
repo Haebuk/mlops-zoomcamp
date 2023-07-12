652093991ab8eedad1d1ee138ce9ee28b55fde0d
Jaesung Ryu <cwryu6252@naver.com>
2023-07-12 20:08:30 +0900

    ci-tests

--- week6/integration-test/run.sh
+++ week6/integration-test/run.sh
@@ -2,13 +2,20 @@
 
 set -e
 
-cd "$(dirname "$0")"
+if [[ -z "${GITHUB_ACTIONS}" ]]; then
+    cd "$(dirname "$0")"
+fi
 
-LOCAL_TAG=`date "+%Y-%m-%d-%H-%M"`
-export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
+    LOCAL_TAG=`date "+%Y-%m-%d-%H-%M"`
+    export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+    export PREDICTIONS_STREAM_NAME="ride_predictions"
+    docker build -t ${LOCAL_IMAGE_NAME} ../
+else
+    echo "Using image ${LOCAL_IMAGE_NAME}"
+fi
+
 export PREDICTIONS_STREAM_NAME="ride_predictions"
-
-docker build -t ${LOCAL_IMAGE_NAME} ../
 
 docker compose up -d
 
